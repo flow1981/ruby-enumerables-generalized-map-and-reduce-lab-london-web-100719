2aa215b8b924e62415bf51c01aac48cd3a5a02ef
Florian Stagliano <contact.f.stagliano@gmail.com>
2019-10-01 14:41:08 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -16,9 +16,9 @@ def map(source_array)
 end
 
 # expect(reduce(source_array){|memo, n| memo + n}).to eq(6)
-  it "returns true when a truthy value is present" do
-    source_array = [ false, nil, nil, nil, true]
-    expect(reduce(source_array){|memo, n| memo || n}).to eq(true)
+# it "returns true when a truthy value is present" do
+# source_array = [ false, nil, nil, nil, true]
+# expect(reduce(source_array){|memo, n| memo || n}).to eq(true)
 
 def reduce(source_array, starting_value=0)
  
