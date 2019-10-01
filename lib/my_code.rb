41831b7aab8437ddeff6b9531dcb0ce56b279cf6
Florian Stagliano <contact.f.stagliano@gmail.com>
2019-10-01 14:35:00 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,21 +15,18 @@ def map(source_array)
   
 end
 
-
+# expect(reduce(source_array){|memo, n| memo + n}).to eq(6)
 
 def reduce(source_array, starting_value=0)
  
- i=0;
- new = starting_value;
+  i=0;
+  new = starting_value;
  
   while i < source_array.length do
     element=source_array[i];
-    i+=1;
     yield (new, element)
+    i+=1;
   end
 
-# expect(reduce(source_array){|memo, n| memo + n}).to eq(6)
-
-#output = source_array.reduce(starting_point) {|result, element| result+=element};
-  output
+  new
 end
