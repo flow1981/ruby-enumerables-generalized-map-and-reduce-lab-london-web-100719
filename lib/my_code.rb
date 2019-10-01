0e5bab5eb7f48ec9268747951bde934a53c84c71
Florian Stagliano <contact.f.stagliano@gmail.com>
2019-10-01 14:06:21 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,35 @@
 # Your Code Here
+
+def map(source_array)
+ 
+  new = [];
+  i = 0;
+  
+  while i < source_array.length do
+    n=source_array[i];
+    new.push(yield (n));
+    i += 1;
+  end
+  
+  new
+  
+end
+
+
+
+def reduce(source_array, starting_value=0)
+ 
+ i=0;
+ new = starting_value;
+ 
+  while i < source_array.length do
+    element=source_array[i];
+    i+=1;
+    yield (element)
+  end
+
+expect(reduce(source_array){|memo, n| memo + n}).to eq(6)
+
+#output = source_array.reduce(starting_point) {|result, element| result+=element};
+  output
+end
